e4728594cd32053c97327e3d7bfd725a4457185e
Matthias Becker <mabecker@kth.se>
2026-07-13 22:22:21 +0200

    Added a script to createthe GUI, to package pyOCD libs

---
+++ createGui.sh
@@ -0,0 +1,13 @@
+CPM_PATH="$(python -c \
+'from PyInstaller.utils.hooks import get_package_paths; print(get_package_paths("cmsis_pack_manager")[1])')"
+
+rm -rf build dist
+
+pyinstaller \
+    src/RT-Trace.py \
+    --noconsole \
+    --icon ./icon/icon.icns \
+    --add-data "Resources/config.ini:." \
+    --collect-all pyocd \
+    --add-data "$CPM_PATH:cmsis_pack_manager" \
+    --noconfirm 
